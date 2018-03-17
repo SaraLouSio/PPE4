0d68a31094cdefc19d4e5bdbfe2202f29c0a4748
SaraLouSio <sara.louafi.66@gmail.com>
2018-03-17 02:57:18 +0100

    css fixé

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Produits;
+/**
+ * Description of ProduitController
+ *
+ * @author Sara
+ */
+class ProduitController extends AbstractController {
+    //put your code here
+    
+     /**
+     * @route("/produit/{id}",name="produit")
+     * @return Response
+     */
+    public function indexController($id) {
+        
+         $produit = $this->getDoctrine()
+                ->getRepository(Produits::class)
+                ->findBy([
+                    'proId' => $id
+                ]);
+        if (!$produit) {
+            throw $this->createNotFoundException(
+                    'Aucun produits'
+            );
+        }
+        
+        return $this->render('home/produit.html.twig', array('produit' => $produit[0]));
+    }
+    
+}
